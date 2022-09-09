6e0e9ee8b5335cd8b0a97f45b2aea723605a4540
Zurisen <mtcarlos98@gmail.com>
2022-09-09 12:05:41 +0200

    python WGAN for batching

---
+++ main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 11:48:23 2022
+
+@author: cmt
+"""
+
+import torch, torchvision, os, PIL, pdb
+from torch import nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import make_grid
+from tqdm.auto import tqdm
+import numpy as np
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+import glob
+from PIL import Image, ExifTags
+import colorsys
+import random
+import pylab
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torchvision.models as models
+import torch.optim as optim
+from tqdm.notebook import tqdm
+import os
+import random
+from IPython import display
+import ignite
+from ignite.engine import Engine, Events
+import ignite.distributed as idist
+from ignite.metrics import FID, InceptionScore
+from ignite.contrib.handlers import ProgressBar
+import warnings
+from utils import *
+from dataloaders import image_loader
+from WGAN import *
+
+
+# NET = argv[1]
+# TAG = argv[2]
+# DATA_PATH = argv[2]
+
+NET = 'WGAN'
+TAG = 'WGAN_spalling'
+DATA_PATH = './data_crops_spalling/imageClustersV130B130/accepted/'
+
+if __name__ == "__main__":
+    
+    image_size = 128 if (NET=="WGAN") else 64
+    z_dim = 200 if (NET=="WGAN") else 100
+    batch_size = 64
+    
+
+    trainset = image_loader(train=True, image_size=image_size,
+                            data_path=DATA_PATH)
+    train_dataloader = DataLoader(trainset, shuffle=True, batch_size=batch_size,
+                                  num_workers=12, drop_last=True)
+    
+    testset = image_loader(train=False, image_size=image_size,
+                           data_path=DATA_PATH)
+    test_dataloader = DataLoader(testset, batch_size=batch_size,
+                                 num_workers=12, drop_last=True)
+    
+    if NET == "WGAN":
+        wgan = train_wgan(train_dataloader, test_dataloader, z_dim, TAG)
+        wgan.run()
+    else:
+        raise NotImplementedError("Unavailable GAN")
+    
+    
+    
+    
---
+++ utils.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 11:00:16 2022
+
+@author: cmt
+"""
+
+import torch, torchvision, os, PIL, pdb
+from torch import nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import make_grid
+from tqdm.auto import tqdm
+import numpy as np
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+import glob
+from PIL import Image, ExifTags
+import colorsys
+import random
+import pylab
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torchvision.models as models
+import torch.optim as optim
+from tqdm.notebook import tqdm
+import os
+import random
+from IPython import display
+import ignite
+from ignite.engine import Engine, Events
+import ignite.distributed as idist
+from ignite.metrics import FID, InceptionScore
+from ignite.contrib.handlers import ProgressBar
+import warnings
+#%%
+
+def show(tensor, num=25, wandbactive=0, name=''):
+    '''
+    Plots grid of input images given Pytorch images batch.
+    '''
+    data = tensor.detach().cpu()
+    grid = make_grid(data[:num], nrow=5).permute(1,2,0)
+
+    plt.imshow(grid.clip(0,1))
+    plt.axis('off')
+    plt.show()
+    plt.axis('off')
+    
+    
+def get_gp(real, fake, crit, alpha, gamma=10):
+    ''' 
+    Calculate Gradient Penalty in WGAN.
+    '''
+    mix_images = real * alpha + fake * (1-alpha) # 128 x 3 x 128 x 128
+    mix_scores = crit(mix_images) # 128 x 1
+
+    gradient = torch.autograd.grad(
+      inputs = mix_images,
+      outputs = mix_scores,
+      grad_outputs=torch.ones_like(mix_scores),
+      retain_graph=True,
+      create_graph=True,
+    )[0] # 128 x 3 x 128 x 128
+
+    gradient = gradient.view(len(gradient), -1)   # 128 x 49152
+    gradient_norm = gradient.norm(2, dim=1) 
+    gp = gamma * ((gradient_norm-1)**2).mean()
+
+    return gp
+
+
+def gen_noise(num, z_dim, device='cuda'):
+    '''
+    Generate noise Pytorch tensor.
+    '''
+    return torch.randn(num, z_dim, device=device) # 128 x 200
+
+
+def interpolate(batch):
+    '''
+    Interpolate dataset images to 299x299x3 to be able to work with 
+    IS and FID prebuilt functions.
+    '''
+    arr = []
+    for img in batch:
+        pil_img = transforms.ToPILImage()(img)
+        resized_img = pil_img.resize((299,299), Image.BILINEAR)
+        arr.append(transforms.ToTensor()(resized_img))
+    return torch.stack(arr)
