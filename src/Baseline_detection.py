53d5d4e7fdabaff035d5d9456ddfd581b5c60680
Carlos Marcos Torrejon <mtcarlos98@gmail.com>
2022-09-16 14:41:12 +0200

    baseline, ODIN and networks

---
+++ src/Baseline_detection.py
@@ -0,0 +1,114 @@
+###################################################################################################
+# Measure the detection performance: reference code is https://github.com/ShiyuLiang/odin-pytorch #
+###################################################################################################
+# Writer: Kimin Lee 
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import data_loader
+import numpy as np
+import torchvision.utils as vutils
+import calculate_log as callog
+import models
+import math
+import os
+
+from torchvision import datasets, transforms
+from torch.nn.parameter import Parameter
+from torch.autograd import Variable
+from numpy.linalg import inv
+
+# Training settings
+parser = argparse.ArgumentParser(description='Test code - measure the detection peformance')
+parser.add_argument('--batch-size', type=int, default=128, help='batch size')
+parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA')
+parser.add_argument('--seed', type=int, default=1,help='random seed')
+parser.add_argument('--dataset', required=True, help='target dataset: Din1')
+parser.add_argument('--dataroot', required=True, help='path to dataset')
+parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the input image to network')
+parser.add_argument('--outf', default='./outf_res', help='folder to output images and model checkpoints')
+parser.add_argument('--out_dataset', required=True, help='out-of-dist dataset: Dout1')
+parser.add_argument('--num_classes', required=True, type=int, default=3, help='number of classes')
+parser.add_argument('--pre_trained_net', required=True, default='', help="path to pre trained_net")
+
+args = parser.parse_args()
+print(args)
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+print("Random Seed: ", args.seed)
+torch.manual_seed(args.seed)
+
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+print('Load model')
+model = models.vgg13(num_classes=args.num_classes)
+model.load_state_dict(torch.load(args.pre_trained_net))
+print(model)
+
+print('load target data: ',args.dataset)
+_, test_loader = data_loader.getTargetDataSet(args.dataset, args.batch_size, 
+        args.imageSize, os.path.join(args.dataroot, args.dataset))
+
+print('load non target data: ',args.out_dataset)
+nt_test_loader = data_loader.getNonTargetDataSet(args.out_dataset, args.batch_size,
+        args.imageSize, os.path.join(args.dataroot, args.out_dataset))
+
+if args.cuda:
+    model.cuda()
+
+def generate_target():
+    model.eval()
+    correct = 0
+    total = 0
+    f1 = open('%s/confidence_Base_In.txt'%args.outf, 'w')
+
+    for data, target in test_loader:
+        total += data.size(0)
+        #vutils.save_image(data, '%s/target_samples.png'%args.outf, normalize=True)
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        batch_output = model(data)
+
+        # compute the accuracy
+        pred = torch.argmax(batch_output.data, dim=1)
+        equal_flag = pred.eq(target.data).cpu()
+        correct += equal_flag.sum()
+        for i in range(data.size(0)):
+            # confidence score: max_y p(y|x)
+            output = batch_output[i].view(1,-1)
+            soft_out = F.softmax(output)
+            soft_out = torch.max(soft_out.data)
+            f1.write("{}\n".format(soft_out))
+
+    print('\n Final Accuracy: {}/{} ({:.2f}%)\n'.format(correct, total, 100. * correct / total))
+
+def generate_non_target():
+    model.eval()
+    total = 0
+    f2 = open('%s/confidence_Base_Out.txt'%args.outf, 'w')
+
+    for data, target in nt_test_loader:
+        total += data.size(0)
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        batch_output = model(data)
+        for i in range(data.size(0)):
+            # confidence score: max_y p(y|x)
+            output = batch_output[i].view(1,-1)
+            soft_out = F.softmax(output)
+            soft_out = torch.max(soft_out.data)
+            f2.write("{}\n".format(soft_out))
+
+print('generate log from in-distribution data')
+generate_target()
+print('generate log  from out-of-distribution data')
+generate_non_target()
+print('calculate metrics')
+callog.metric(args.outf)
---
+++ src/models.py
@@ -0,0 +1,151 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+import os
+
+
+class VGG(nn.Module):
+
+    def __init__(self, features, num_classes=10):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(512, num_classes),
+        )
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+def make_layers(cfg):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+def vgg13(pretrained=False, **kwargs):
+    """VGG 13-layer model (configuration "B")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = VGG(make_layers(cfg['B']), **kwargs)
+    return model
+
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+class _netD(nn.Module):
+    def __init__(self, ngpu, nc, ndf):
+        super(_netD, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input size. (nc) x 32 x 32
+            nn.Conv2d(nc, ndf * 2, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        return output.view(-1, 1)
+
+class _netG(nn.Module):
+    def __init__(self, ngpu, nz, ngf, nc):
+        super(_netG, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, nc, 4, 2, 1, bias=False),
+            nn.Sigmoid()
+            # state size. (nc) x 32 x 32
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
+
+def Generator(n_gpu, nz, ngf, nc):
+    model = _netG(n_gpu, nz, ngf, nc)
+    model.apply(weights_init)
+    return model
+
+def Discriminator(n_gpu, nc, ndf):
+    model = _netD(n_gpu, nc, ndf)
+    model.apply(weights_init)
+    return model
+
